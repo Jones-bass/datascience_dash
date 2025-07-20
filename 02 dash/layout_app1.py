e39b31fe98ab5f8674dcb8d1de988164f76ca540
jones-bass <jonesbass.tb@gmail.com>
2025-07-20 19:32:41 -0300

    Ajuste Layout 1 add Layout 2

--- 02 dash/layout_app1.py
+++ 02 dash/layout_app1.py
@@ -7,8 +7,11 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#f5f7fa',
+    'text': '#2c3e50',
+    'header': '#2980b9',
+    'card': '#ffffff',
+    'graph_bg': '#ecf0f1'
 }
 
 df = pd.DataFrame({
@@ -18,32 +21,39 @@ df = pd.DataFrame({
 })
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
 fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    plot_bgcolor=colors['graph_bg'],
+    paper_bgcolor=colors['card'],
+    font_color=colors['text'],
+    legend_bgcolor=colors['graph_bg'],
+    legend_borderwidth=1
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
+app.layout = html.Div(style={'backgroundColor': colors['background'], 'padding': '30px'}, children=[
+    html.Div([
+        html.H1('Hello Dash', style={
             'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+            'color': colors['header'],
+            'margin': '0',
+            'padding': '0'
+        }),
+        html.H3('Dash: Um framework web para Python.', style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'margin': '0',
+            'padding': '0'
+        }),
+    ]),
 
-    html.Div(children='Dash: Um framework web para Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+    html.Div([
+        dcc.Graph(id='example-graph-2', figure=fig)
+    ], style={
+        'backgroundColor': colors['card'],
+        'borderRadius': '8px',
+        'boxShadow': '0 8px 8px 0 rgba(0,0,0,0.1)',
+        'margin': '0 auto'
+    })
 ])
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
